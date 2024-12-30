7eafbece39988fc8818fba4f570212c8ba180742
Uktra <uktra.ds@gmail.com>
2024-12-30 21:54:06 +0200

    ..

--- src/app/modules/home/page/american-school/academic-program/academic-program.component.ts
+++ src/app/modules/home/page/american-school/academic-program/academic-program.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-academic-program',
   templateUrl: './academic-program.component.html',
-  styleUrls: ['./academic-program.component.scss']
+  styleUrls: ['./academic-program.component.scss'],
 })
 export class AcademicProgramComponent {
-
+  constructor(private sharedService: SharedService) {}
 }
